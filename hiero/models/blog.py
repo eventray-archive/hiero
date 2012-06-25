e687f0d1d9e82f4d8c9b9506388ba55bccd7d2ea
John Anderson <sontek@gmail.com>
2012-06-25 13:07:01 -0500

    added comments of what we are going to do for the blog

---
+++ hiero/models/blog.py
@@ -0,0 +1,11 @@
+# Series
+    # A look up table of posts in a Series
+    # title, description, posts
+
+# Entry
+    # The blog post itself
+    # owner, series, is_featured, title, creator_ip, is_draft, allow_comments,
+    # slug, content, html_content, markup type, category, tags, created_on,
+    # related_entries
+
+    # SEO stuff - keywords, description
---
+++ hiero/views/blog.py
@@ -0,0 +1,8 @@
+# index
+    # list of entries, paged.
+
+# detail
+    # detail view of a specific entry
+
+# search
+    # full text search of entries
