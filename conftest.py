0266063c4d8419221ab501407eac25d4eb4f0a9d
John Anderson <sontek@gmail.com>
2012-07-06 22:10:06 -0500

    throw exit 1 since py.test hides it

--- conftest.py
+++ conftest.py
@@ -1,28 +1,30 @@
 import os
+import sys
+
 from pkg_resources          import resource_filename
 
 def pytest_sessionstart():
-    from py.test import config
+    try:
+        from py.test import config
 
-    # Only run database setup on master (in case of xdist/multiproc mode)
-    if not hasattr(config, 'slaveinput'):
-        from pyramid.config         import Configurator
-        from hiero.tests.models     import Base
-        from paste.deploy.loadwsgi  import appconfig
-        from sqlalchemy             import engine_from_config
+        # Only run database setup on master (in case of xdist/multiproc mode)
+        if not hasattr(config, 'slaveinput'):
+            from pyramid.config         import Configurator
+            from hiero.tests.models     import Base
+            from paste.deploy.loadwsgi  import appconfig
+            from sqlalchemy             import engine_from_config
 
-        settings = appconfig('config:' + resource_filename(__name__,
-            'hiero/tests/test.ini')
-        )
-        engine = engine_from_config(settings, prefix='sqlalchemy.')
+            settings = appconfig('config:' + resource_filename(__name__,
+                'hiero/tests/test.ini')
+            )
+            engine = engine_from_config(settings, prefix='sqlalchemy.')
 
-        print 'Creating the tables on the test database %s' % engine
+            print 'Creating the tables on the test database %s' % engine
 
-        config = Configurator(settings=settings)
-        config.scan('horus.models')
+            config = Configurator(settings=settings)
+            config.scan('horus.models')
 
-        Base.metadata.drop_all(engine)
-        Base.metadata.create_all(engine)
-
-
-
+            Base.metadata.drop_all(engine)
+            Base.metadata.create_all(engine)
+        except:
+            sys.exit(1)
