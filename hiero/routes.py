8d7a2105638f5f30bc928b9b5d43c2a8613efe15
John Anderson <sontek@gmail.com>
2012-07-10 06:04:12 -0500

    better url

--- hiero/routes.py
+++ hiero/routes.py
@@ -7,7 +7,7 @@ def includeme(config):
     config.add_route('hiero_admin_entry_index_paged',   '/admin/page/{page}')
     config.add_route('hiero_admin_entry_create',   '/admin/new_entry')
     config.add_route('hiero_admin_entry_edit'
-           , '{slug}/edit'
+           , '/admin/{slug}/edit'
            , factory=EntryFactory
            , traverse="/{slug}"
     )
