ad18eabd17d64c427d5fa1241c0d987b7610a49a
John Anderson <sontek@gmail.com>
2012-07-29 17:10:35 -0500

    bump version

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ requires = [
 
 setup(
     name='hiero'
-    , version='0.2'
+    , version='0.3'
     , description='hiero'
     , long_description=README + '\n\n' +  CHANGES
     , classifiers=[
